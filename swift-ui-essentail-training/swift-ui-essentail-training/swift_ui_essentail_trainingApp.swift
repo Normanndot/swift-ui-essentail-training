b79ee91306df30e27839e187282970a70614833b
Norman <normanndot@gmail.com>
2021-01-26 15:52:17 +0530

    Initial commit

---
+++ swift-ui-essentail-training/swift-ui-essentail-training/swift_ui_essentail_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_essentail_trainingApp.swift
+//  swift-ui-essentail-training
+//
+//  Created by Norman, ThankaVijay on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_essentail_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
